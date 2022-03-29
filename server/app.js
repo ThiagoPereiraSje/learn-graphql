860b88c80baea5e683fb79876cddf54549c8fe8b
Thiago Pereira <thiagopereiramail@gmail.com>
2022-03-29 14:03:33 +0000

    Import the scheme on express server

--- server/app.js
+++ server/app.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
 
 const app = express();
 
 // Setup a middleware that translate Rest to GraphQl
-app.use("/graphql", graphqlHTTP({}));
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+  })
+);
 
+// Start express server
 app.listen(4000, () => {
   console.log("now listening for requests on port 4000");
 });
