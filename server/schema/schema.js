e49c5ce309b80cb928d9ab520872ef20197be12f
Thiago Pereira <thiagopereiramail@gmail.com>
2022-03-29 20:04:31 +0000

    Implement books resolve method

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,14 @@
 const graphql = require("graphql");
+const lodash = require("lodash");
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+// Mock some data
+const books = [
+  { name: "Name of the Wind", genre: "Fantasy", id: "1" },
+  { name: "The Final Empire", genre: "Fantasy", id: "2" },
+  { name: "The Long Earth", genre: "Sci-Fi", id: "3" },
+];
 
 const BookType = new GraphQLObjectType({
   name: "Book",
@@ -20,6 +28,7 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLString } },
       resolve(parent, args) {
         // Code to get data from DB/other source
+        return lodash.find(books, { id: args.id });
       },
     },
   },
