f9c869484ab25b0faef7a555ba620df41094f670
Thiago Pereira <thiagopereiramail@gmail.com>
2022-03-30 12:24:23 +0000

    Create bidirectional relationship

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -6,6 +6,7 @@ const {
   GraphQLID,
   GraphQLString,
   GraphQLInt,
+  GraphQLList,
   GraphQLSchema,
 } = graphql;
 
@@ -14,6 +15,10 @@ const books = [
   { name: "Name of the Wind", genre: "Fantasy", id: "1", authorId: "1" },
   { name: "The Final Empire", genre: "Fantasy", id: "2", authorId: "2" },
   { name: "The Long Earth", genre: "Sci-Fi", id: "3", authorId: "3" },
+
+  { name: "The Hero of Ages", genre: "Fantasy", id: "4", authorId: "2" },
+  { name: "The Colour of Magic", genre: "Fantasy", id: "5", authorId: "3" },
+  { name: "The Light Fantastic", genre: "Fantasy", id: "6", authorId: "3" },
 ];
 
 const authors = [
@@ -45,6 +50,14 @@ const AuthorType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     age: { type: GraphQLInt },
+    books: {
+      type: new GraphQLList(BookType),
+
+      // Now parent is a Author
+      resolve(parent, args) {
+        return lodash.filter(books, { authorId: parent.id });
+      },
+    },
   }),
 });
 
