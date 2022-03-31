8260307be16ee8890209f47a651f07e037ecaea4
Thiago Pereira <thiagopereiramail@gmail.com>
2022-03-31 15:24:38 +0000

    Setup Apollo Client for React

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import BookList from "./components/BookList";
 export default function App() {
   return (
     <div>
-      <h1>Ninja's Book List</h1>
+      <h1>Ninja's Reading List</h1>
       <BookList />
     </div>
   );
