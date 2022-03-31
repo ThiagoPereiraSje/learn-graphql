46a7e38f63c9c0c10d4f30a74dca4f20f91157f5
Thiago Pereira <thiagopereiramail@gmail.com>
2022-03-31 15:09:49 +0000

    Create the first frontend component. The BookList

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,10 @@
+import BookList from "./components/BookList";
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1>Ninja's Book List</h1>
+      <BookList />
+    </div>
+  );
 }
---
+++ client/src/components/BookList.jsx
@@ -0,0 +1,10 @@
+export default function BookList() {
+  return (
+    <div>
+      Book List
+      <ul id="book-list">
+        <li>Book name</li>
+      </ul>
+    </div>
+  );
+}
