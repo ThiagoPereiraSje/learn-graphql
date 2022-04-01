276e8d73d93ddc302c15f88347631e453e40ce57
Thiago Pereira <thiagopereiramail@gmail.com>
2022-04-01 18:45:47 +0000

    Show book details

--- client/src/components/BookDetails.jsx
+++ client/src/components/BookDetails.jsx
@@ -1,10 +1,30 @@
-// import { getBookQuery } from "../queries/queries";
+import { getBookQuery } from "../queries/queries";
 
-export default function BookDetails() {
+import { useQuery } from "@apollo/client";
+
+export default function BookDetails({ bookId }) {
+  const query = useQuery(getBookQuery, { variables: { id: bookId } });
+
+  if (query.loading) return <p>Loading...</p>;
+  if (query.error) return <p>{!bookId ? "Select a Book" : "Oops! :("}</p>;
+
+  const book = query.data.book;
+
   return (
     <div id="book-details">
-      <h1>Book Name</h1>
-      <p>Output book details here!</p>
+      <h1>{book.name}</h1>
+      <p>{book.genre}</p>
+      <p>
+        <b>Author: </b>
+        {book.author.name}
+      </p>
+
+      <b>Books:</b>
+      <ul>
+        {book.author.books.map((b) => (
+          <li key={b.id}>{b.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
--- client/src/components/BookList.jsx
+++ client/src/components/BookList.jsx
@@ -1,16 +1,18 @@
 import { useQuery } from "@apollo/client";
+import { useState } from "react";
 import { getBooksQuery } from "../queries/queries";
 import BookDetails from "./BookDetails";
 
 export default function BookList() {
   const { loading, error, data } = useQuery(getBooksQuery);
+  const [bookId, setBookId] = useState("");
 
   const displayBooks = () => {
     if (loading) return <>Loading books...</>;
     if (error) return <>Oops! :(</>;
 
     return data.books.map((book) => (
-      <li key={book.id}>
+      <li key={book.id} onClick={() => setBookId(book.id)}>
         {book.name} - {book.genre}
       </li>
     ));
@@ -23,7 +25,7 @@ export default function BookList() {
         <ul id="book-list">{displayBooks()}</ul>
       </div>
       <div>
-        <BookDetails />
+        <BookDetails bookId={bookId} />
       </div>
     </div>
   );
